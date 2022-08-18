f98a16ca5c4e2f1d5ef22e3c3d4a7b7d216595e3
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2022-08-18 09:52:55 +0900

    remove `BinaryTree` and add `BinarySearchTree`

---
+++ kotlin/src/main/kotlin/me/yapoo/tree/BinarySearchTree.kt
@@ -0,0 +1,136 @@
+package me.yapoo.tree
+
+import kotlin.math.max
+
+
+class BinarySearchTree<T : Comparable<*>> {
+
+    var root: Node<T>? = null
+
+    fun find(x: T): Node<T>? {
+        var w = root
+        while (w != null) {
+            val comp = compareValues(x, w.value)
+            w = if (comp < 0) {
+                w.left
+            } else if (comp > 0) {
+                w.right
+            } else {
+                return w
+            }
+        }
+        return null
+    }
+
+    fun add(x: T): Boolean {
+        val parent = findLast(x)
+        if (parent == null) {
+            root = Node(x)
+            return true
+        }
+
+        val comp = compareValues(x, parent.value)
+        if (comp == 0) {
+            return false
+        }
+
+        val new = Node(x)
+        new.parent = parent
+        if (comp < 0) {
+            parent.left = new
+        } else {
+            parent.right = new
+        }
+        return true
+    }
+
+    fun remove(x: T): Boolean {
+        val node = find(x) ?: return false
+        if (node.left == null || node.right == null) {
+            splice(node)
+        } else {
+            var w: Node<T>? = node.right
+            while (w?.left != null) {
+                w = w.left
+            }
+
+            node.value = w!!.value
+            splice(w)
+        }
+
+        return true
+    }
+
+    // x 以下の最大の値を持つノードを返却
+    private fun findLast(x: T): Node<T>? {
+        var current = root
+        var prev: Node<T>? = null
+        while (current != null) {
+            prev = current
+
+            val comp = compareValues(x, current.value)
+            current = if (comp < 0) {
+                current.left
+            } else if (comp > 0) {
+                current.right
+            } else {
+                return current
+            }
+        }
+
+        return prev
+    }
+
+    // 葉または子を 1 つだけ持つノードを削除する
+    private fun splice(u: Node<T>) {
+        val s = if (u.left != null) {
+            u.left
+        } else {
+            u.right
+        }
+
+        if (u.parent == null) {
+            root = s
+            root?.parent = null
+        } else {
+            val p = u.parent!!
+            if (p.left == u) {
+                p.left = s
+            } else {
+                p.right = s
+            }
+            if (s != null) {
+                s.parent = p
+            }
+        }
+    }
+}
+
+class Node<T : Comparable<*>>(
+    var value: T,
+    var parent: Node<T>? = null,
+    var left: Node<T>? = null,
+    var right: Node<T>? = null,
+) {
+    fun depth(): Int {
+        if (this.parent == null) {
+            return 0
+        }
+
+        return 1 + this.parent!!.depth()
+    }
+
+    fun size(): Int {
+        val left = left?.size() ?: 0
+        val right = right?.size() ?: 0
+
+        return 1 + left + right
+    }
+
+    fun height(): Int {
+        val left = left?.height() ?: 0
+        val right = right?.height() ?: 0
+
+        return 1 + max(left, right)
+    }
+}
--- kotlin/src/test/kotlin/me/yapoo/tree/BinaryTreeTest.kt
+++ kotlin/src/test/kotlin/me/yapoo/tree/BinaryTreeTest.kt
@@ -2,33 +2,96 @@ package me.yapoo.tree
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class BinaryTreeTest {
 
     @Test
     fun test() {
-        val nodes = List(11) { BinaryTree(it) }
-        nodes[0].setLeft(nodes[1])
-        nodes[0].setRight(nodes[6])
-        nodes[1].setLeft(nodes[2])
-        nodes[1].setRight(nodes[3])
-        nodes[3].setLeft(nodes[4])
-        nodes[3].setRight(nodes[5])
-        nodes[6].setLeft(nodes[7])
-        nodes[6].setRight(nodes[8])
-        nodes[7].setLeft(nodes[9])
-        nodes[8].setLeft(nodes[10])
+        val tree = BinarySearchTree<Int>()
+        val values = List(1000) { it }.shuffled()
 
-        assertEquals(11, nodes[0].size())
-        assertEquals(4, nodes[0].height())
-        assertEquals(0, nodes[0].depth())
+        values.forEach {
+            assertTrue { tree.add(it) }
+            assertEquals(it, tree.find(it)?.value)
+        }
+        values.forEach {
+            assertTrue { tree.remove(it) }
+            assertNull(tree.find(it))
+        }
+    }
 
-        assertEquals(5 , nodes[1].size())
-        assertEquals(3, nodes[1].height())
-        assertEquals(1, nodes[1].depth())
+    @Test
+    fun tree_structure() {
+        val tree = BinarySearchTree<Int>()
+        val values = listOf(7, 3, 11, 1, 5, 9, 13, 0, 2, 4, 6, 8, 10, 12, 14)
 
-        assertEquals(1, nodes[10].size())
-        assertEquals(1, nodes[10].height())
-        assertEquals(3, nodes[10].depth())
+        values.forEach {
+            tree.add(it)
+        }
+
+        assertNull(tree.find(7)?.parent)
+        assertEquals(3, tree.find(7)?.left?.value)
+        assertEquals(11, tree.find(7)?.right?.value)
+        assertEquals(0, tree.find(7)?.depth())
+        assertEquals(15, tree.find(7)?.size())
+        assertEquals(4, tree.find(7)?.height())
+
+        assertEquals(7, tree.find(3)?.parent?.value)
+        assertEquals(1, tree.find(3)?.left?.value)
+        assertEquals(5, tree.find(3)?.right?.value)
+
+        assertEquals(7, tree.find(11)?.parent?.value)
+        assertEquals(9, tree.find(11)?.left?.value)
+        assertEquals(13, tree.find(11)?.right?.value)
+
+        assertEquals(3, tree.find(1)?.parent?.value)
+        assertEquals(0, tree.find(1)?.left?.value)
+        assertEquals(2, tree.find(1)?.right?.value)
+
+        assertEquals(3, tree.find(5)?.parent?.value)
+        assertEquals(4, tree.find(5)?.left?.value)
+        assertEquals(6, tree.find(5)?.right?.value)
+
+        assertEquals(11, tree.find(9)?.parent?.value)
+        assertEquals(8, tree.find(9)?.left?.value)
+        assertEquals(10, tree.find(9)?.right?.value)
+
+        assertEquals(11, tree.find(13)?.parent?.value)
+        assertEquals(12, tree.find(13)?.left?.value)
+        assertEquals(14, tree.find(13)?.right?.value)
+
+        assertEquals(1, tree.find(0)?.parent?.value)
+        assertNull(tree.find(0)?.left)
+        assertNull(tree.find(0)?.right)
+
+        assertEquals(1, tree.find(2)?.parent?.value)
+        assertNull(tree.find(2)?.left)
+        assertNull(tree.find(2)?.right)
+
+        assertEquals(5, tree.find(4)?.parent?.value)
+        assertNull(tree.find(4)?.left)
+        assertNull(tree.find(4)?.right)
+
+        assertEquals(5, tree.find(6)?.parent?.value)
+        assertNull(tree.find(6)?.left)
+        assertNull(tree.find(6)?.right)
+
+        assertEquals(9, tree.find(8)?.parent?.value)
+        assertNull(tree.find(8)?.left)
+        assertNull(tree.find(8)?.right)
+
+        assertEquals(9, tree.find(10)?.parent?.value)
+        assertNull(tree.find(10)?.left)
+        assertNull(tree.find(10)?.right)
+
+        assertEquals(13, tree.find(12)?.parent?.value)
+        assertNull(tree.find(12)?.left)
+        assertNull(tree.find(12)?.right)
+
+        assertEquals(13, tree.find(14)?.parent?.value)
+        assertNull(tree.find(14)?.left)
+        assertNull(tree.find(14)?.right)
     }
 }
