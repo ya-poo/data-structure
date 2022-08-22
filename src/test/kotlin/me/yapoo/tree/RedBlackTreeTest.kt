7c548df7b26c9f5e8f2491a99860b4cb122163f0
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2022-08-22 13:47:51 +0900

    fix `RedBlackTreeTest`

--- src/test/kotlin/me/yapoo/tree/RedBlackTreeTest.kt
+++ src/test/kotlin/me/yapoo/tree/RedBlackTreeTest.kt
@@ -5,6 +5,7 @@ import java.util.*
 import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
@@ -15,7 +16,6 @@ class RedBlackTreeTest {
     fun test() {
         val tree = RedBlackTree<Int>()
         val values = List(1000) { it }.shuffled()
-        println(values)
 
         values.forEach {
             assertTrue { tree.add(it) }
@@ -25,6 +25,7 @@ class RedBlackTreeTest {
             assertBlackHeight(tree)
             assertNoRedEdge(tree)
             assertLeftLeaning(tree)
+            assertNoDoubleBlack(tree)
         }
 
         values.shuffled().forEach {
@@ -36,6 +37,7 @@ class RedBlackTreeTest {
             assertBlackHeight(tree)
             assertNoRedEdge(tree)
             assertLeftLeaning(tree)
+            assertNoDoubleBlack(tree)
         }
     }
 
@@ -87,6 +89,8 @@ class RedBlackTreeTest {
                 assertContains(listOf(RedBlackTree.Color.Black, null), node.left?.color)
                 assertContains(listOf(RedBlackTree.Color.Black, null), node.right?.color)
             }
+            node.left?.let { stack.push(it) }
+            node.right?.let { stack.push(it) }
         }
     }
 
@@ -103,6 +107,23 @@ class RedBlackTreeTest {
             if (node.left == null || node.left?.color == RedBlackTree.Color.Black) {
                 assertContains(listOf(RedBlackTree.Color.Black, null), node.right?.color)
             }
+            node.left?.let { stack.push(it) }
+            node.right?.let { stack.push(it) }
+        }
+    }
+
+    private fun assertNoDoubleBlack(
+        tree: RedBlackTree<*>
+    ) {
+        val root = tree.root ?: return
+
+        val stack = Stack<RedBlackTree.Node<*>>()
+        stack.push(root)
+        while (stack.isNotEmpty()) {
+            val node = stack.pop()
+            assertNotEquals(RedBlackTree.Color.DoubleBlack, node.color)
+            node.left?.let { stack.push(it) }
+            node.right?.let { stack.push(it) }
         }
     }
 }
